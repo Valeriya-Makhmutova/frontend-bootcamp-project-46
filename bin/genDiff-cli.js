3c244b8f8193b35943044f1cc15851e0186f548f
Valeriya Makhmutova <mavapa369.yandex.ru>
2023-07-27 20:14:49 +0500

    updated genDiff

--- bin/genDiff-cli.js
+++
@@ -1,5 +0,0 @@
-import file1 from './file1.json'; // при таком способе испорта данные парсятся автоматически, и нет необходимости использовать 
-import file2 from './file2.json'; // JSON.parse(): . file1 и fail2 находятся в виде объектов
-import generateDifference from '../src/gendiff';
-
-generateDifference(file1, file2);
--- src/gendiff.js
+++ src/gendiff.js
@@ -2,23 +2,53 @@
 
 const { Command } = require('commander');
 const program = new Command();
-import _ from 'lodash';
+const _ = require('lodash');
+const fs = require('fs');
+const path = require('path')
 
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.8.0')
-  .argument('<filepath1>')
-  .argument('<filepath2>')
+  .argument('filepath1')
+  .argument('filepath2')
   .option('-f, --format <type>', 'output format')
 
-  program.parse();
 
-export default function generateDifference(file1, file2) {
-  const keysFile1 = Object.keys(file1); // помещаем ключи объекта file1 в массив keysFile1
-  const keysFile2 = Object.keys(file2); // помещаем ключи объекта file2 в массив keysFile2
-  const unitedKeys = _.union(keysFile1, keysFile2); // объединяем ключи в новый массив, без повторяющихся ключей
+program.command('generateDifference')
+  .description('Search differences and matches between two files and display it as string')
+  .argument('<file1>')
+  .argument('<file2>')
+  .option('--first', 'display just the first substring')
+  .action((file1, file2) => {
+    const file1Content = JSON.parse(fs.readFileSync(path.resolve(file1), 'utf8'));
+    const file2Content = JSON.parse(fs.readFileSync(path.resolve(file2), 'utf8'));
 
-}
+    let result = '';
+    const keysFile1 = Object.keys(file1Content); // помещаем ключи объекта file1 в массив keysFile1
+    const keysFile2 = Object.keys(file2Content); // помещаем ключи объекта file2 в массив keysFile2
+    const unitedKeys = _.sortBy(_.union(keysFile1, keysFile2)); // объединяем ключи в новый массив, без повторяющихся ключей
+    // unitedKeys - [host, timeout, proxy, follow, verbose] и сортируем в алфавитном порядке
+    unitedKeys.forEach((key) => {
+      if (keysFile1.includes(key) && !keysFile2.includes(key)) {
+        result += ` - ${key}: ${file1Content[key]}\n`;
+      }
+      if (keysFile1.includes(key) && keysFile2.includes(key) && file1Content[key] === file2Content[key]) {
+        result += `   ${key}: ${file1Content[key]}\n`;
+      }
+      if (keysFile1.includes(key) && keysFile2.includes(key) && file1Content[key] !== file2Content[key]) {
+        result += ` - ${key}: ${file1Content[key]}\n`;
+        result += ` + ${key}: ${file2Content[key]}\n`;
+      }
+      if (!keysFile1.includes(key) && keysFile2.includes(key)) {
+        result += ` + ${key}: ${file2Content[key]}\n`;
+      }
+    });
+    console.log(`{\n${result}}`);
+    return `{\n${result}}`;
+  });
+
+
+program.parse();
 
 
 
