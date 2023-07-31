c9d8d02e90579ea2646c3005a27c930a8ce98e19
Valeriya Makhmutova <mavapa369.yandex.ru>
2023-07-31 19:04:23 +0500

    added tests

--- _tests_/get-difference.test.js
+++ _tests_/get-difference.test.js
@@ -1,8 +1,21 @@
 const getDifference = require('../src/get-difference');
-const file1 = require('../src/file1.json');
-const file2 = require('../src/file2.json');
-
+const file2 = process.cwd() + '/src/file2.json';
+const file1 = process.cwd() + '/src/file1.json';
+console.log("🚀 ~ file: get-difference.test.js:5 ~ file1:", file1)
 
 test('type of function result', () => {
     expect(typeof getDifference(file1, file2)).toBe('string');
-  });
+});
+
+const result = `{
+ - follow: false
+   host: hexlet.io
+ - proxy: 123.234.53.22
+ - timeout: 50
+ + timeout: 20
+ + verbose: true
+}`;
+
+test('work of function', () => {
+    expect(getDifference(file1, file2)).toBe(result);
+});
