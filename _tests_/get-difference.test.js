b7a087200f437529b2d7977b73d15bd9be4fc242
Valeriya Makhmutova <mavapa369.yandex.ru>
2023-08-04 18:11:05 +0500

    add yml files

--- _tests_/get-difference.test.js
+++ _tests_/get-difference.test.js
@@ -9,7 +9,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-test('type of function result', () => {
+
+test('type of function result json', () => {
     expect(typeof getDifference(getFixturePath("file1.json"), getFixturePath("file2.json"))).toBe('string');
 });
 
@@ -22,6 +23,14 @@ const result = `{
  + verbose: true
 }`;
 
-test('work of function', () => {
+test('work of function with json', () => {
     expect(getDifference(getFixturePath("file1.json"), getFixturePath("file2.json"))).toBe(result);
+});
+
+test('type of function result yml', () => {
+  expect(typeof getDifference(getFixturePath("file1.yml"), getFixturePath("file2.yml"))).toBe('string');
+});
+
+test('work of function with yml', () => {
+  expect(getDifference(getFixturePath("file1.yml"), getFixturePath("file2.yml"))).toBe(result);
 });
