2582d2e04d7f5a261699c4334277f5d456aaf99e
Valeriya Makhmutova <mavapa369.yandex.ru>
2025-01-28 01:12:37 +0500

    added compare of flat objects

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,6 +2,11 @@
 
 import {Command} from 'commander';
 const program = new Command();
+import fs from 'fs';
+import path from 'path';
+import { readFileSync } from 'node:fs';
+import { cwd } from 'node:process';
+import getDifference from '../src/index.js';
 
 program
   .name('gendiff')
@@ -10,9 +15,23 @@ program
   .argument('filepath1', 'path to the first file to compare')
   .argument('filepath2', 'path to the second file to compare')
   .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2) => {
+    const getAbsolutePath = (filepath) => {
+      return path.resolve(cwd(),filepath);
+    }
+    const data1 = fs.readFileSync(getAbsolutePath(filepath1));
+    const data2 = fs.readFileSync(getAbsolutePath(filepath2));
+
+    const parsedFirstFile = JSON.parse(data1);
+    const parsedSecondFile = JSON.parse(data2);
+    // console.log(JSON.parse(data1));
+    // console.log(JSON.parse(data2));
+    console.log(getDifference(parsedFirstFile, parsedSecondFile));
+    
+  })
   .parse(process.argv);
 
-program.parse();
+// program.parse();
 
-program.version('0.0.1', '-V, --version', 'output the current version');
+// program.version('0.0.1', '-V, --version', 'output the current version');
 
---
+++ src/index.js
@@ -0,0 +1,69 @@
+import _ from 'lodash';
+
+const getDifference = (obj1, obj2) => {
+  const lines = [];
+  const signs = ['-', '+', ' '];
+  const [minus, plus, space] = signs;
+
+  const keysOfObj1 = Object.keys(obj1);
+  const keysOfObj2 = Object.keys(obj2);
+
+  const unionKeys = _.union(keysOfObj1, keysOfObj2);
+  const sortedKeys = _.sortBy(unionKeys);
+  
+  for (const key of sortedKeys) {
+    const isOnlyInFirst = Object.hasOwn(obj1, key) && !Object.hasOwn(obj2, key);
+    const isOnlyInSecond = !Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key);
+    const isBothTrue = Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key) && obj1[key] === obj2[key];
+    const isBothFalse = Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key) && obj1[key] !== obj2[key];
+
+
+    if (isOnlyInFirst) {
+      lines.push(`  ${minus} ${key}: ${obj1[key]}`);
+    }
+
+    if (isOnlyInSecond) {
+      lines.push(`  ${plus} ${key}: ${obj2[key]}`);
+    }
+    
+    if (isBothTrue) {
+      lines.push(`  ${space} ${key}: ${obj1[key]}`);
+    }
+
+    if (isBothFalse) {
+      lines.push(`  ${minus} ${key}: ${obj1[key]}`);
+      lines.push(`  ${plus} ${key}: ${obj2[key]}`);
+    }
+  }
+
+  const result = ['{',...lines,'}'].join('\n');
+  
+  return result;
+};
+
+export default getDifference;
+// const stringify = (value, replacer = ' ', spacesCount = 1) => {
+//   const iter = (currentValue, depth) => {
+//     // альтернативный вариант: (typeof currentValue !== 'object' || currentValue === null)
+//     if (!_.isObject(currentValue)) {
+//       return `${currentValue}`;
+//     }
+
+//     const indentSize = depth * spacesCount;
+//     const currentIndent = replacer.repeat(indentSize);
+//     const bracketIndent = replacer.repeat(indentSize - spacesCount);
+//     const lines = Object
+//       .entries(currentValue)
+//       .map(([key, val]) => `${currentIndent}${key}: ${iter(val, depth + 1)}`);
+
+//     return [
+//       '{',
+//       ...lines,
+//       `${bracketIndent}}`,
+//     ].join('\n');
+//   };
+
+//   return iter(value, 1);
+// };
+
+// export default stringify;
